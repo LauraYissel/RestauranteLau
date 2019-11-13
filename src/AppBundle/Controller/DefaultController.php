cafd7accd043126c9acd703fe6548932dbd4f080
Laura <luaana788@gmail.com>
2019-11-13 13:58:02 +0100

    changes

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Restaurante;
+use AppBundle\Entity\Platos;
+use Doctrine\Common\Collections\ArrayCollection;
+
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+     
+
+    public function indexAction(Request $request)
+    {
+        return $this->render('default/index.html.twig');
+            
+        
+    }
+     /** 
+    * @Route("/restaurante", name="restaurante");
+    */
+     
+
+    public function restauranteAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository(Restaurante::class);
+        $restaurantes = $repository->findAll();
+        dump($restaurantes);
+         return $this->render('default/restaurante.html.twig',['restaurantes'=>$restaurantes]);
+    }
+    /** 
+    * @Route("/restaurante/{$sede}", name="sedes");
+    */
+     
+
+    public function sedesAction(Request $request, $sede)
+    {
+       
+        return $this->render('default/restaurante.html.twig',['sede'=>$sede]);
+    }
+    
+    /** 
+    * @Route("restaurante/contacto", name="contacto");
+    */
+     
+
+    public function contactoAction(Request $request)
+    {
+        
+         return $this->render('default/contacto.html.twig');
+    }
+
+    // /** 
+    // * @Route("/platos", name="platos");
+    // */
+     
+
+    // public function platosAction(Request $request)
+    // {
+    //     $repository = $this->getDoctrine()->getRepository(Restaurante::class);
+    //     $restaurante = $repository->find(1);
+
+    //     $platoFirst = new Platos();
+    //     $platoFirst->setNombre('Tortilla');
+    //     $platoFirst->setDescripcion('The best!');
+
+    //     // relates this plato to the restaurante
+    //     $platoFirst->setrestaurante($restaurante);
+
+    //     $entityManager = $this->getDoctrine()->getManager();
+    //     $entityManager->persist($platoFirst);
+    //     $entityManager->flush();
+
+    //     return new Response(
+    //         $platoFirst->getNombre()
+    //     );
+    // }
+    
+}
+
+
+
+
+
---
+++ src/AppBundle/Form/PlatosType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Form;
+
+
+use AppBundle\Entity\Restaurante;
+use AppBundle\Form\PlatosType;
+use AppBundle\Form\RestauranteType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class PlatosType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('nombre', TextType::class)
+        ->add('descripcion', TextType::class)
+        ->add('restaurante', EntityType::class, [
+            // looks for choices from this entity
+            'class' => Restaurante::class,
+        
+            // uses the User.username property as the visible option string
+            'choice_label' => 'nombre',
+        
+            // used to render a select box, check boxes or radios
+            // 'multiple' => true,
+            // 'expanded' => true,
+        ]);
+        // ->add('save', SubmitType::class, ['label' => 'Crear Plato']);
+        
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Platos'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_platos';
+    }
+
+    public function __toString(){
+        return '';
+    }
+}
